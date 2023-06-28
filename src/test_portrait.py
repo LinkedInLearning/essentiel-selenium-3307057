803d50d44043c30c87723a6032d218914b67a34e
Labasse <labasse@gmail.com>
2023-06-29 01:59:25 +0200

    Sélectionner une valeur (début)

--- src/test_portrait.py
+++ src/test_portrait.py
@@ -12,8 +12,27 @@ def teardown_function():
 
 def test_selection_partie():
     partie = Select(driver.find_element(By.ID, "partie"))
+    
     partie.select_by_visible_text("Œil")
+    
     fruits = driver.find_elements(By.CSS_SELECTOR, "#palette .fruit")
     ids = [f.get_attribute("id") for f in fruits]
     assert ids == ["1-3", "3-1", "2-1"]
 
+def test_diminution_taille():
+    """ Test de la diminution de la valeur à 3 """
+    taille = driver.find_element(By.ID, "taille")
+    # Action pour passer à 3
+    assert driver.find_element(By.ID, "valeur").text == str(3)
+
+def test_augmentation_taille():
+    """ Test de l'augmentation de la valeur à 8 """
+    pass
+
+def test_minimum_taille():
+    """ Test du passage à la valeur minimale: 0 """
+    pass
+
+def test_maximum_taille():
+    """ Test du passage à la valeur maximale: 9 """
+    pass
