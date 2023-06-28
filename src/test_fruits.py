8e072f4a6dbfc2ede9e50656928d83c5a3365310
Labasse <labasse@gmail.com>
2023-06-29 01:43:56 +0200

    Cibler un élément (fin)

--- src/test_fruits.py
+++ src/test_fruits.py
@@ -1,21 +1,25 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 
 def setup_function():
     global driver
     driver = Chrome()
-
+    driver.get("https://labasse.github.io/fruits/")
+    
 def teardown_function():
     driver.quit()
 
 def test_init_titre():
-    driver.get("https://labasse.github.io/fruits/")
     assert driver.title == "Salade de fruits"
 
 def test_init_chercher_actif():
-    pass
+    chercher = driver.find_element(By.LINK_TEXT, "Chercher")
+    assert "active" in chercher.get_attribute("class")
 
 def test_init_liste_complete():
-    pass
+    cartes = driver.find_elements(By.CLASS_NAME, "card")
+    assert len(cartes) == 11
 
 def test_init_focus_recherche():
-    pass
+    recherche = driver.find_element(By.CSS_SELECTOR, "input[type=search]")
+    assert recherche == driver.switch_to.active_element
