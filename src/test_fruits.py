171142a81f3ad1c73da0cdc818c385e1c9eba42a
Labasse <labasse@gmail.com>
2023-06-29 01:45:21 +0200

    Simuler le clavier et la souris (fin)

--- src/test_fruits.py
+++ src/test_fruits.py
@@ -1,5 +1,6 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 def setup_function():
     global driver
@@ -12,6 +13,16 @@ def teardown_function():
 def find_recherche():
     return driver.find_element(By.CSS_SELECTOR, "input[type=search]")
 
+def ecrire_recherche(texte):
+    recherche = find_recherche()
+    recherche.click()
+    recherche.send_keys(texte)
+    return recherche
+
+def assert_nombre_cartes(attendu):
+    cartes = driver.find_elements(By.CLASS_NAME, "card")
+    assert len(cartes) == attendu
+
 def test_init_titre():
     assert driver.title == "Salade de fruits"
 
@@ -20,21 +31,22 @@ def test_init_chercher_actif():
     assert "active" in chercher.get_attribute("class")
 
 def test_init_liste_complete():
-    cartes = driver.find_elements(By.CLASS_NAME, "card")
-    assert len(cartes) == 11
+    assert_nombre_cartes(11)
 
 def test_init_focus_recherche():
     recherche = find_recherche()
     assert recherche == driver.switch_to.active_element
 
 def test_recherche_fructueuse():
-    recherche = find_recherche()
-    pass
+    ecrire_recherche("ana")
+    assert_nombre_cartes(2)
 
 def test_recherche_infructueuse():
-    recherche = find_recherche()
-    pass
+    ecrire_recherche("radis")
+    assert_nombre_cartes(0)
 
 def test_recherche_raz():
-    recherche = find_recherche()
-    pass
+    recherche = ecrire_recherche("radis")
+    for i in range(5):
+        recherche.send_keys(Keys.BACKSPACE)
+    assert_nombre_cartes(11)
