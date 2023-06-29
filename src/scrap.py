c8dc83abd485d7f2cf6f7a8a03c57446460a54f7
Labasse <labasse@gmail.com>
2023-06-29 11:52:20 +0200

    Faire du scrapping (fin)

--- src/scrap.py
+++ src/scrap.py
@@ -5,7 +5,14 @@ from selenium.webdriver.common.by import By
 def main() -> int:
     """ Extraction des fruits du site et de leur description """
     url = "https://labasse.github.io/fruits/"
-    
+    driver = Chrome()
+    driver.get(url)
+    cartes = driver.find_elements(By.CLASS_NAME, "card")
+    for c in cartes:
+        nom = c.find_element(By.CLASS_NAME, "card-title").text
+        img = c.find_element(By.TAG_NAME, "img").get_attribute("src")[len(url):]
+        txt = c.find_element(By.CLASS_NAME, "card-text" ).text
+        print(f"- {nom: <10}{img:<20}{txt}")
     return 0
 
 if __name__ == '__main__':
