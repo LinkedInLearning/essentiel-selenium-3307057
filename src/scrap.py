ca73d5b3791d309a998b5aa529903c19b47b2ac5
Labasse <labasse@gmail.com>
2023-06-29 11:51:31 +0200

    Faire du scrapping (début)

---
+++ src/scrap.py
@@ -0,0 +1,12 @@
+import sys
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
+def main() -> int:
+    """ Extraction des fruits du site et de leur description """
+    url = "https://labasse.github.io/fruits/"
+    
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main()) 
