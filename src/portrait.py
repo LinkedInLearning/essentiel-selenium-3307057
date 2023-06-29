4802d0856776d1c5982524ad2ab5f72c47f850e5
Labasse <labasse@gmail.com>
2023-06-29 02:18:33 +0200

    Suivre les mouvements (début)

--- src/portrait.py
+++ src/portrait.py
@@ -1,21 +1,28 @@
-from selenium.webdriver import ActionChains
+from selenium.webdriver import Chrome, Firefox, Edge, ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.remote.webelement import WebElement
+from os import path
+import json, math
 
 class Page:
-    def __init__(self, driver, baseurl):
+    def __init__(self, driver: Chrome or Firefox or Edge, baseurl: str):
+        """ Charge la page grâce au driver selenium en partant de l'url spécifié """
         driver.get(f"{baseurl}/portrait.html")
         self.driver = driver
         self.taille   = driver.find_element(By.ID, "taille")
         self.canevas  = driver.find_element(By.ID, "canevas")
 
-    def glisser_deplacer(self, id):
+    def glisser_deplacer(self, id: str) -> None:
+        """ Déplace le fruit d'identifiant id au centre du portrait """
         ActionChains(self.driver).drag_and_drop(
             self.driver.find_element(By.ID, id),
             self.driver.find_element(By.ID, "canevas")
         ).perform()
 
-    def glisser_fruit(self, id, x, y, zoom):
+    def glisser_fruit(self, id: str, x: int, y: int, zoom: int) -> None:
+        """ Glisse le fruit d'identifiant id à l'offset x, y du portrait avec le zoom spécifié """
         fruit = self.driver.find_element(By.ID, id)
         dx = self.canevas.location['x'] + x - fruit.location['x']
         dy = self.canevas.location['y'] + y - fruit.location['y']
@@ -23,3 +30,58 @@ class Page:
             .drag_and_drop_by_offset(fruit, dx, dy)\
             .send_keys_to_element(self.taille, Keys.HOME+Keys.ARROW_RIGHT*zoom)\
             .perform()
+    
+    def redimmensionner(self, largeur: int, hauteur: int) -> None:
+        """ Redimensionne la page aux largeur et hauteur spécifiées """
+        self.driver.set_window_size(largeur, hauteur)
+
+    def lire_js_fruits(self) -> dict:
+        """ Retourne un dictionnaire de la donnée interne javascript 'fruits'  """
+        return json.loads(self.driver.execute_script("return JSON.stringify(fruits)"))
+    
+    def deselectionner(self) -> None:
+        """ Désélectionne le fruit sur le portrait """
+        self.driver.execute_script("select(null)")
+
+    def fruits_du_portrait(self) -> list[WebElement]:
+        """ Retourne une liste des div correspondant aux fruits qui composent le portrait """
+        return self.driver.find_elements(By.CSS_SELECTOR, "div.fruit")
+    
+    def fruits_de_la_palette(self) -> list[WebElement]:
+        """ Retourne une liste des li correspondant aux fruits qui composent la palette """
+        return self.driver.find_elements(By.CSS_SELECTOR, "#palette .fruit")
+    
+    def filtrer_palette(self, filtre: str) -> None:
+        """ Filtre les fruits de la palette, 'Tout' pour retirer le filtre """
+        Select(self.driver.find_element(By.ID, "partie")).select_by_visible_text(filtre)
+
+    def get_titre(self) -> WebElement:
+        """ Retourne le composant 'Titre' de la page """
+        return self.driver.find_element(By.NAME, "titre")
+    
+    def get_taille(self) -> WebElement:
+        """ Retourne le composant 'Taille' de la page """
+        return self.driver.find_element(By.ID, "taille")
+    
+    def get_texte_taille(self) -> str:
+        """ Retourne le texte du composant 'Taille' de la page """
+        return self.driver.find_element(By.ID, "valeur").text
+
+    def charger_fond(self, fichier: str) -> None:
+        """ Charge l'image spécifiée pour le fond du portrait """
+        self.driver.find_element(By.ID, "fond").send_keys(path.abspath(fichier))
+
+    def _coin(self, delta: int, taille: int) -> int:
+        """ Détermine la coordonnée extrême à utiliser en 
+            fonction de la dimension du portrait et du signe du delta 
+        """
+        return (5-taille/2)*math.copysign(1, delta) 
+
+    def deplacer_fond(self, dx: int, dy: int) -> None:
+        """ Décale le fond du portrait à la souris de la distance spécifiée par dx et dy """
+        coin_x = self._coin(self.canevas.rect['width' ], dx)
+        coin_y = self._coin(self.canevas.rect['height'], dy)
+        # Cliquer et garder enfoncé sur un coin
+        # Déplacer sur dx et dy
+        # Relacher
+            
--- src/test_portrait.py
+++ src/test_portrait.py
@@ -1,14 +1,11 @@
-from selenium.webdriver import Chrome, ActionChains
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from os import path
-import pyperclip, json, portrait
+import pyperclip, portrait
 
 def _test_changement_de_taille(raccourci, valeur):
-    taille = driver.find_element(By.ID, "taille")
-    taille.send_keys(raccourci)
-    assert driver.find_element(By.ID, "valeur").text == str(valeur)
+    page.get_taille().send_keys(raccourci)
+    assert page.get_texte_taille() == str(valeur)
 
 def setup_function():
     global driver, page
@@ -20,12 +17,8 @@ def teardown_function():
 
 def test_selection_partie():
     """ Test de la sélection d'Œil """
-    partie = Select(driver.find_element(By.ID, "partie"))
-    
-    partie.select_by_visible_text("Œil")
-    
-    fruits = driver.find_elements(By.CSS_SELECTOR, "#palette .fruit")
-    ids = [f.get_attribute("id") for f in fruits]
+    page.filtrer_palette("Œil")
+    ids = [f.get_attribute("id") for f in page.fruits_de_la_palette()]
     assert ids == ["1-3", "3-1", "2-1"]
 
 def test_diminution_taille():
@@ -46,44 +39,35 @@ def test_maximum_taille():
 
 def test_televersement_fond():
     """ Test du téléversement d'une image de fond """
-    fond = driver.find_element(By.ID, "fond")
-    fond.send_keys(path.abspath("banner.jpg"))
+    page.charger_fond("banner.jpg")
     assert "data:image/jpeg;base64" in driver.find_element(By.ID, "canevas").get_attribute("style")
 
 def test_glisser_deplacer_fruit_sur_canevas():
     """ Glisser/Déplace le premier fruit de la palette sur le Canevas """
-    fruit   = driver.find_element(By.ID, "0-0")
-    canevas = driver.find_element(By.ID, "canevas")
-    
-    actions = ActionChains(driver)
-#    actions.drag_and_drop(fruit, canevas).perform()
-    actions.drag_and_drop_by_offset(fruit,
-        canevas.rect['x'] + canevas.rect['width' ]/2 - fruit.location['x'], 
-        canevas.rect['y'] + canevas.rect['height']/2 - fruit.location['y']
-    ).perform()
-    portrait = driver.find_elements(By.CSS_SELECTOR, "div.fruit")
+    page.glisser_deplacer("0-0")
+    portrait = page.fruits_du_portrait()
     assert len(portrait)==1
     assert "cur" in portrait[0].get_attribute("class")
 
 def test_coller_titre():
     """ Test de collage du presse-papier dans titre """
     pyperclip.copy("Hommage à Arcimboldo")
-    titre = driver.find_element(By.NAME, "titre")
+    titre = page.get_titre()
     titre.send_keys(Keys.CONTROL, "v")
     assert titre.get_attribute('value') == "Hommage à Arcimboldo"
 
 def test_js_select():
     """ Test de la désélection javascript select(null) """
     page.glisser_deplacer("0-0")
-    driver.execute_script("select(null)")
-    portrait = driver.find_elements(By.CSS_SELECTOR, "div.fruit")
+    page.deselectionner()
+    portrait = page.fruits_du_portrait()
     assert "cur" not in portrait[0].get_attribute("class")
 
 def test_lecture_donnees_javascript():
     """ Test du contenu de la variable fruit """
-    driver.set_window_size(1280, 720)
+    page.redimmensionner(1280, 720)
     page.glisser_deplacer("0-0")
-    donnees_fruits = json.loads(driver.execute_script("return JSON.stringify(fruits)"))
+    donnees_fruits = page.lire_js_fruits()
     assert donnees_fruits == {
         "f1": {
             "icone":"0-0",
@@ -95,15 +79,13 @@ def test_lecture_donnees_javascript():
 
 def test_dessin_complexe():
     """ Test d'un dessin complexe avec capture du résultat """
-    driver.set_window_size(1280, 720)
-    taille   = driver.find_element(By.ID, "taille")
-    canevas  = driver.find_element(By.ID, "canevas")
-
+    page.redimmensionner(1280, 720)
+    page.charger_fond("banner.jpg")
+    page.deplacer_fond(-200, -100)
     page.glisser_fruit("4-2",  18,  68, 9)
     page.glisser_fruit("2-0", 163, 203, 7)
     page.glisser_fruit("1-2", 162, 269, 7)
     page.glisser_fruit("1-3",  92, 145, 6)
     page.glisser_fruit("1-3", 253, 145, 6)
-    driver.execute_script("select(null)")
-    driver.find_element(By.ID, "fond").send_keys(path.abspath("banner.jpg"))
-    driver.save_screenshot("dessing.jpg")
+    page.deselectionner()
+    driver.save_screenshot("dessin.png")
