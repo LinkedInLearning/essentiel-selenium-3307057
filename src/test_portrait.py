aa234df49a586798a2eff8e215580c4434c18f51
Labasse <labasse@gmail.com>
2023-06-29 02:00:21 +0200

    Sélectionner une valeur (fin)

--- src/test_portrait.py
+++ src/test_portrait.py
@@ -1,6 +1,12 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+
+def _test_changement_de_taille(raccourci, valeur):
+    taille = driver.find_element(By.ID, "taille")
+    taille.send_keys(raccourci)
+    assert driver.find_element(By.ID, "valeur").text == str(valeur)
 
 def setup_function():
     global driver
@@ -21,18 +27,16 @@ def test_selection_partie():
 
 def test_diminution_taille():
     """ Test de la diminution de la valeur à 3 """
-    taille = driver.find_element(By.ID, "taille")
-    # Action pour passer à 3
-    assert driver.find_element(By.ID, "valeur").text == str(3)
+    _test_changement_de_taille(Keys.ARROW_LEFT*2, 3)
 
 def test_augmentation_taille():
     """ Test de l'augmentation de la valeur à 8 """
-    pass
+    _test_changement_de_taille(Keys.ARROW_RIGHT*3, 8)
 
 def test_minimum_taille():
     """ Test du passage à la valeur minimale: 0 """
-    pass
+    _test_changement_de_taille(Keys.HOME, 0)
 
 def test_maximum_taille():
     """ Test du passage à la valeur maximale: 9 """
-    pass
+    _test_changement_de_taille(Keys.END, 9)
